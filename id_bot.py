962d55b446964791165d6d4a44ed6f5146c65ae7
mk7k <makskohut135@gmail.com>
2025-06-14 21:52:26 +0000

    quote_bot

--- id_bot.py
+++ id_bot.py
@@ -3,9 +3,14 @@ from aiogram.types import Message
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram import F
-
+import aiohttp
 import asyncio
 from aiogram.client.bot import DefaultBotProperties
+import ssl
+
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 BOT_TOKEN = "7968332774:AAHZi-GKP-QzVzmTau_tUBk1PL6HyfyMJ_I"
 
@@ -19,6 +24,20 @@ async def start_handler(message: Message):
 @dp.message(F.text.lower().contains("привіт"))
 async def hello_handler(message: Message):
     await message.answer("Привіт, чемпіоне 😎")
+
+@dp.message(F.text == "/цитата")
+async def send_quote(message: types.Message):
+    try:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+            async with session.get("https://api.quotable.io/random") as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                    quote = f"“{data['content']}”\n— {data['author']}"
+                    await message.answer(quote)
+                else:
+                    await message.answer("🚫 Не вдалося отримати цитату.")
+    except Exception as e:
+        await message.answer(f"⚠️ Помилка: {e}")
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
